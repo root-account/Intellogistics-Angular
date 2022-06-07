bb4bdb626979ac6fba6462817ce0bfe5c04e283d
ephraim <ephraim@hellocavalry.com>
2022-06-07 08:05:57 +0200

    Comment out profile page

--- customer-dash/src/app/layouts/admin-layout/admin-layout.module.ts
+++ customer-dash/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,7 +7,7 @@ import { ClipboardModule } from 'ngx-clipboard';
 
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+// import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // import { ToastrModule } from 'ngx-toastr';
 
@@ -22,7 +22,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   declarations: [
     DashboardComponent,
-    UserProfileComponent,
+    // UserProfileComponent,
   ]
 })
 
